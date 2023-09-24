c322eba273fdbb00d5eec70d5d7e27018fc78b00
gamzemeryemkaya <gamzemeryemkaya@gmail.com>
2023-09-24 23:11:12 +0300

    customer layout düzenlendi

---
+++ EasyCashIdentityProject.PresentationLayer/ViewComponents/Customer/_CustomerLayoutHeaderPartial.cs
@@ -0,0 +1,12 @@
+﻿﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.ViewComponents.Customer
+{
+    public class _CustomerLayoutHeaderPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

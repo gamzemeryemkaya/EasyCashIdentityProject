66f7b4928bf4311f03030587db758efd8c72ef07
gamzemeryemkaya <gamzemeryemkaya@gmail.com>
2023-09-28 23:56:53 +0300

    para transfer işlemleri

--- EasyCashIdentityProject.DataAccessLayer/Migrations/20230928140218_mig_add_CustomerAccountProcess_desc.Designer.cs
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20230928140218_mig_add_CustomerAccountProcess_desc.Designer.cs
@@ -4,6 +4,7 @@ using EasyCashIdentityProject.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyCashIdentityProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230928140218_mig_add_CustomerAccountProcess_desc")]
+    partial class mig_add_CustomerAccountProcess_desc
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/SendMoneyController.cs
@@ -0,0 +1,62 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using EasyCashIdentityProject.DtoLayer.Dtos.CustomerAccountProcessDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class SendMoneyController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ICustomerAccountProcessService _customerAccountProcessService;
+        public SendMoneyController(UserManager<AppUser> userManager, ICustomerAccountProcessService customerAccountProcessService)
+        {
+            _userManager = userManager;
+            _customerAccountProcessService = customerAccountProcessService;
+        }
+
+        [HttpGet]
+        public IActionResult Index(string mycurrency)
+        {
+            ViewBag.currency = mycurrency;
+            return View();
+        }
+
+
+        //alıcı ve gönderen hesapların kimliklerini belirlemek ve bir para gönderme işlemi için gerekli verileri hazırlamak 
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SendMoneyForCustomerAccountProcessDto sendMoneyForCustomerAccountProcessDto)
+        {
+            // 1. Veritabanı bağlamını oluşturun
+            var context = new Context();
+
+            // 2. Kullanıcıyı bulun (oturum açmış kullanıcıyı temsil eder)
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            // 3. Alıcı hesabın kimliğini hesaplayın
+            var receiverAccountNumberID = context.CustomerAccounts
+                .Where(x => x.CustomerAccountNumber == sendMoneyForCustomerAccountProcessDto.ReceiverAccountNumber)
+                .Select(y => y.CustomerAccountID)
+                .FirstOrDefault();
+
+            // 5. Para gönderme işlemi için bir nesne oluşturun ve verileri doldurun
+            var values = new CustomerAccountProcess();
+            values.ProcessDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            values.ProcessType = "Havale";
+            values.SenderID = user.Id;
+            values.ReceiverID = receiverAccountNumberID;
+            values.Amount = sendMoneyForCustomerAccountProcessDto.Amount;
+
+            // 6. İşlemi veritabanına kaydedin
+            _customerAccountProcessService.TInsert(values);
+
+            // 7. Kullanıcıyı başka bir ekrana yönlendirin
+            return RedirectToAction("Index", "Deneme");
+        }
+
+    }
+}
